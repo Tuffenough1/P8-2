2e8d93e349b1a122c284acff2eeda855a8fdbdab
Tuff Woodruff <t-woodruff.2@onu.edu>
2024-10-25 22:05:54 -0400

    completed countWords function.

--- P8-2/P8-2.cpp
+++ P8-2/P8-2.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int countWords(string str);
+
+int main() {
+    string input;
+
+    cout << "Enter a string: ";
+    getline(cin, input);
+
+    int wordCount = countWords(input);
+    cout << "Number of words: " << wordCount << endl;
+
+    return 0;
+}
+
+int countWords(string str) {
+    int count = 0;
+    bool inWord = false;
+
+    for (char ch : str) {
+        if (ch != ' ') {
+            if (!inWord) {
+                count++;
+                inWord = true;
+            }
+        }
+        else {
+            inWord = false;
+        }
+    }
+
+    return count;
+}
